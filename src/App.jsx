9bce408e36b70ca1e3549924ece5c74614406855
Harinirpm <harinirangammal4@gmail.com>
2024-09-28 21:16:43 +0530

    skills modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import HorizontalNavbar from './Navbar/HorizontalNavbar';
 import Home from './Components/Home';
 import About from './Components/About';
-import Skills from './Components/Skills';
+// import Skills from './Components/Skills';
 import Project from './Components/Project';
 import Contact from './Components/Contact';
 import SideNavbar from './Navbar/SideNavbar'
@@ -22,9 +22,9 @@ function App() {
           <div id="about">
             <About />
           </div>
-          <div id="skills">
+          {/* <div id="skills">
             <Skills />
-          </div>
+          </div> */}
           <div id="projects">
             <Project />
           </div>
